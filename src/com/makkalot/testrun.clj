f8f45f0b2998a2ee05597d2906cbcdf6c210fdb0
makkalot <makkalot@gmail.com>
2013-11-18 18:04:24 +0200

    First commit of cljs-autotest lein plugin

---
+++ src/com/makkalot/testrun.clj
@@ -0,0 +1,122 @@
+(ns com.makkalot.testrun
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell  :refer [sh]])
+  (:use [leiningen.core.eval :only [eval-in-project]])
+  (:import [java.util.concurrent ScheduledThreadPoolExecutor TimeUnit]
+           [java.io File]))
+
+;scheduling api
+(def scheduled-futures (atom {}))
+
+(defn stop [service-tag]
+  (if-let [future (service-tag @scheduled-futures)]
+    (.cancel future true)
+    (swap! scheduled-futures dissoc service-tag)))
+
+(defn schedule [service-tag function interval]
+  (when (@scheduled-futures service-tag)
+    (stop service-tag))
+  (let [executor (ScheduledThreadPoolExecutor. 1)
+        function (bound-fn [] (function))
+        future (.scheduleWithFixedDelay executor function 0 interval TimeUnit/MILLISECONDS)]
+    (swap! scheduled-futures assoc service-tag future)))
+
+;file operations here
+(defn create-tracker []
+  {})
+
+(defn create-tracker-atom []
+  (atom (create-tracker)))
+
+(def current-tracker (atom (create-tracker)))
+
+
+(defn find-files [dirs file-filter-fn]
+  (->> dirs
+       (map io/file)
+       (filter #(.exists ^File %))
+       (mapcat file-seq)
+       (filter file-filter-fn)
+       (map #(.getCanonicalFile ^File %))))
+
+(defn js-file?
+  "Returns true if the java.io.File represents a normal javascript
+  file."
+  [^java.io.File file]
+  (and (.isFile file)
+       (.endsWith (.getName file) ".js")))
+
+
+(defn find-js-files
+  "What it gets is absolute paths of files not directories"
+  [dirs]
+  (find-files dirs js-file?))
+
+
+(defn modified-files [tracker files]
+  (filter #(< (::time tracker 0) (.lastModified ^File %)) files))
+
+(defn update-files [tracker modified]
+  (let [now (System/currentTimeMillis)]
+    (-> tracker
+        (update-in [::files] (fnil into #{}) modified)
+        (assoc ::time now))))
+
+(defn get-tracker-time [tracker]
+  (::time tracker))
+
+
+(defn get-tracker-files [tracker]
+  (::files tracker))
+
+(defn scan [tracker dirs]
+   (let [ds (seq dirs)
+        files (find-js-files ds)
+        modified (seq (modified-files tracker files))]
+    modified))
+
+
+(defn react-on-change! [tracker modified]
+  ;you should do your reload magic here !
+  (println "Modified files ..." modified))
+
+
+(defn scan-and-react! [tracker options scanner & {:keys [react-fn] :or {react-fn react-on-change!}}]
+  (swap! tracker
+       #(let [modified (scanner % (:files options))]
+         (if modified
+           (do
+             (react-fn % modified)
+             (update-files % modified))
+           %))))
+
+
+(defn get-file-list [test-options]
+  (filter #(.endsWith % ".js") test-options))
+
+(defn react-fn-runner
+  "Runs the commands for the modified test suite"
+  [test-commands tracker modified]
+  (let [result (apply sh test-commands)]
+    (println "RESULT " result))
+  (println "TEST-COMMANDS " test-commands)
+  (println "MODIFIED " modified))
+
+(defn autotest
+  "autest method"
+  [test-mode options interval]
+  (println "AUTOTEST " test-mode options interval)
+  (let [track-files (get-file-list options)]
+    (schedule
+      :autotest
+      #(scan-and-react!
+        current-tracker
+        {:files track-files}
+        scan
+        :react-fn (partial react-fn-runner options))
+      interval)
+    (while true
+      (Thread/sleep 15000)
+      (println "Checking ...")
+      (println (.get (:autotest @scheduled-futures))))))
+
---
+++ test/com/makkalot/testrun/test_auto.clj
@@ -0,0 +1,155 @@
+(ns com.makkalot.testrun.test-auto
+  (:require [me.raynes.fs :as fs]
+            [clojure.string :refer [join]]
+            [com.makkalot.testrun :refer
+             [find-js-files create-tracker modified-files
+              update-files get-tracker-time get-tracker-files
+              scan scan-and-react! create-tracker-atom]])
+  
+  (:use clojure.test)
+                     
+  (:import [java.io File]))
+
+
+(defn- left-strip-path [^String s]
+  (if (.startsWith s File/separator)
+    (apply str (rest s))
+    s))
+
+(defn- right-strip-path [^String s]
+  (if (.endsWith s File/separator)
+    (apply str (butlast s))
+    s))
+
+
+(defn- strip-path [^String s]
+  (let [left (left-strip-path s)
+        right (right-strip-path left)]
+    right))
+
+
+(defn- join-path [base-path & paths]
+  (let [stripped-paths (map strip-path paths)
+        bpath (right-strip-path base-path)]
+    (join File/separator (cons bpath stripped-paths))))
+
+(defn- generate-n-js-files [abs-path n]
+  (for [i (range n)]
+    (let [path (join-path abs-path (str i ".js"))
+          f (fs/file path)]
+     f)))
+
+(defn- create-n-js-files!
+  "You should note that we have here side effects so doseq"
+  [abs-path n]
+  (let [files (generate-n-js-files abs-path n)]
+    (doseq [f files]
+      (fs/create f))
+    files))
+
+
+(defn get-file-list
+  "Gets all of the files of the directory"
+  [dir]
+  ;(println "Brinign what ! " dir)
+  (let [files (map
+               #(join-path dir %1)
+               (fs/list-dir dir))]
+        files))
+
+;created during fixtures
+(declare ^:dynamic cur-files)
+(declare ^:dynamic cur-dir)
+
+ 
+(defn tmp-file-fixture [f]
+  (let [tmpdir (fs/temp-dir "testrun")
+        abs-path (.getAbsolutePath tmpdir)
+        files (create-n-js-files! abs-path 3)]
+    (try
+      (binding [cur-files files
+                cur-dir abs-path]
+        (f))
+      (finally
+        (fs/delete-dir abs-path)))))
+
+
+(deftest test-find-files
+  (let [found-files (find-js-files (get-file-list cur-dir))]
+                                        ;check the number of files
+    (is (== (count cur-files)
+            (count found-files)))
+    ;check if files are same
+    (is (= (set
+            (map #(.getAbsolutePath %) cur-files))
+           (set
+            (map #(.getAbsolutePath %) found-files))))))
+
+
+
+
+(deftest test-modify
+  (let [tracker (create-tracker)
+        found-files (find-js-files (get-file-list cur-dir))]
+    (is (== (count found-files)
+            (count
+             (modified-files tracker found-files))))))
+
+
+(deftest test-modify-update
+  (let [tracker (create-tracker)
+        found-files (find-js-files (get-file-list cur-dir))
+        updated-tracker (update-files tracker found-files)
+        to-modify (first found-files)]
+    (is (not
+         (nil?
+          (get-tracker-time updated-tracker))))
+    (is (= (count found-files)
+           (count
+            (get-tracker-files updated-tracker))))
+    ;touch the file so we can see if it is detected
+    (fs/touch (.getAbsolutePath to-modify)
+              (+ 1000 (System/currentTimeMillis)))
+   
+    (let [re-file (first
+                   (find-js-files
+                    [(.getAbsolutePath to-modify)]))
+          modified-fs (modified-files updated-tracker found-files)]
+
+      (is (== 1 (count modified-fs)))
+      (is (= (.getAbsolutePath
+              (first modified-fs))
+             (.getAbsolutePath to-modify))))))
+
+(defn react-fn-test [mark-atom tracker modified]
+  (swap! mark-atom update-in [:files] into modified))
+
+(deftest test-scan
+  (let
+      [tracker (create-tracker-atom)
+       file-list (get-file-list cur-dir)
+       to-modify (first file-list)
+       react-atom (atom {})]
+    
+    (is (= (count
+            (scan tracker file-list))
+           (count file-list)))
+
+                                        ;Lets update the tracker here
+    (scan-and-react! tracker {:files file-list} scan)
+    (fs/touch to-modify
+              (+ 1000 (System/currentTimeMillis)))
+
+    (scan-and-react!
+     tracker
+     {:files file-list}
+     scan
+     :react-fn (partial react-fn-test react-atom))
+    
+                                        ;now check if the file is same
+    (is (= to-modify
+           (.getAbsolutePath (first (@react-atom :files)))))))
+
+
+
+(use-fixtures :each tmp-file-fixture)
