19115f8720d3a7f9463cd58436157d947232945d
makkalot <makkalot@gmail.com>
2013-11-24 20:27:45 +0200

    More cleaning

--- src/com/makkalot/testrun.clj
+++ src/com/makkalot/testrun.clj
@@ -89,29 +89,3 @@
 
 
 
-(defn react-fn-runner
-  "Runs the commands for the modified test suite"
-  [test-commands tracker modified]
-  (let [result (apply sh test-commands)]
-    (println "RESULT " result))
-  (println "TEST-COMMANDS " test-commands)
-  (println "MODIFIED " modified))
-
-(defn autotest
-  "autotest method"
-  [test-mode options interval]
-  (println "AUTOTEST " test-mode options interval)
-  (let [track-files (get-file-list options)]
-    (schedule
-      :autotest
-      #(scan-and-react!
-        current-tracker
-        {:files track-files}
-        scan
-        :react-fn (partial react-fn-runner options))
-      interval)
-    (while true
-      (Thread/sleep 15000)
-      (println "Checking ...")
-      (println (.get (:autotest @scheduled-futures))))))
-
